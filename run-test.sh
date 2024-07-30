4ce3b6770198081b8d5cdf7a51d46824492f5d37
Wes Garland <wes@distributive.network>
2024-07-30 16:47:01 -0400

    comment out DCP_DEBUG because it is currently broken

--- run-test.sh
+++ run-test.sh
@@ -4,5 +4,5 @@ echo "Please ensure the DCP system at `node -e "require('dcp-client').init().the
 echo "and that there is a working evaluator on your system"
 echo
 
-export DCP_DEBUG=${DCP_DEBUG:-all:all}
+#export DCP_DEBUG=${DCP_DEBUG:-all:all}
 node ./deploy-and-run-simple-job.simple
